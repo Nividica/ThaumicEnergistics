2529ce25fad4e140414df1a510353da31dc387c4
sww1235 <sww1235@users.noreply.github.com>
2015-01-31 12:14:54 -0700

    Update AEAspectRegister.java

--- src/main/java/thaumicenergistics/registries/AEAspectRegister.java
+++ src/main/java/thaumicenergistics/registries/AEAspectRegister.java
@@ -247,7 +247,7 @@ public class AEAspectRegister
 			}
 
 			// Inscriber recipes
-			for( InscriberRecipe recipe : Inscribe.RECIPIES )
+			for( InscriberRecipe recipe : Inscribe.RECIPES )
 			{
 				try
 				{
