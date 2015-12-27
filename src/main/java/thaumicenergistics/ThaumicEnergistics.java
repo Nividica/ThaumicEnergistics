1d4700f6b185883a9ec6f9e2cd1d163e1f54b6b0
Chris <Nividica@gmail.com>
2015-12-27 07:34:52 -0500

    Version bump
    Moving from alpha to beta.

--- src/main/java/thaumicenergistics/ThaumicEnergistics.java
+++ src/main/java/thaumicenergistics/ThaumicEnergistics.java
@@ -42,7 +42,7 @@ public class ThaumicEnergistics
 	/**
 	 * Current version of the mod.
 	 */
-	public static final String VERSION = "0.9.0.4"; // Note: don't forget to change the build.gradle file as well
+	public static final String VERSION = "0.9.1.0"; // Note: don't forget to change the build.gradle file as well
 
 	/**
 	 * Singleton instance
