f74836352e1ff2e6e412b44afd0c867e7cdefa51
BrockWS <ddoggy99@gmail.com>
2019-05-01 00:27:02 +1000

    Version 2.2.2

--- src/main/java/thaumicenergistics/init/ModGlobals.java
+++ src/main/java/thaumicenergistics/init/ModGlobals.java
@@ -14,7 +14,7 @@ public class ModGlobals {
 
     public static final String MOD_ID = "thaumicenergistics";
     public static final String MOD_NAME = "Thaumic Energistics";
-    public static final String MOD_VERSION = "2.2.1";
+    public static final String MOD_VERSION = "2.2.2";
     public static final String MOD_DEPENDENCIES = "required-after:appliedenergistics2@[rv6-stable-6,);" +
             "required-after:thaumcraft@[6.1.BETA26,);" +
             "after:thaumicjei";
