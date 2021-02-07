66b6a1194cfa3f477bd62eb178dc1a03d063e95e
Alex811 <811alex@gmail.com>
2021-02-07 20:03:22 +0200

    Add missing API interfaces

---
+++ src/api/java/thaumicenergistics/api/IThETextures.java
@@ -0,0 +1,15 @@
+package thaumicenergistics.api;
+
+import net.minecraft.util.ResourceLocation;
+
+/**
+ * Contains functions that return the ResourceLocation for textures in Thaumic Energistics
+ *
+ * @author Alex811
+ * @version 1.0.1
+ * @since 1.0.1
+ */
+public interface IThETextures {
+
+    ResourceLocation knowledgeCoreSlot();
+}
